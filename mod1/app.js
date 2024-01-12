10cc22005c784ece9ee5a219f34502701db47401
satyaa <satyasingh525553@gmail.com>
2024-01-12 23:57:00 +0530

    module3 mid

--- mod1/app.js
+++ mod1/app.js
@@ -1,24 +1,22 @@
 import React from "react";
-// const punycode = require('punycode/');
 import ReactDOM from "react-dom/client";
-// const heading = React.createElement("h1", { id: "heading", class: "satyaa" }, "Hello World!");
 
-// const root = ReactDOM.createRoot(document.querySelector(".root"));
-// root.render(heading);
+const Heading = () => {
+    return (<h1 id="title">Satya welcomes you</h1>)
+}
 
-/* <div id="parent">
-    <div id="child1">
-        <h1>heading 1</h1>
-    </div>
-    <div id="child2">
-        <h2>heading 2</h2>
-    </div>
-</div> */
+const Child1 = () => {
+    return (<div id="child1">Nesting</div>);
+}
 
-const par = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child1" }, React.createElement("h1", {}, "heading 1")), React.createElement("div", { id: "child2" }, React.createElement("h2", {}, "heading 2"))]);
-
+const Maincon = () => {
+    return (
+        <div className="parent">
+            <Heading />
+            <Child1 />
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.querySelector(".root"));
-// React.render
-root.render(par);
+root.render(<Maincon />);
